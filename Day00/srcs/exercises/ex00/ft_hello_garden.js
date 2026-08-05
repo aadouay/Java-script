c7b906fdaac822ca0db164cea88d1deeefc7cc43
Ayoub Adouay <aadouay>
2026-08-05 11:30:32 +0100

    ex00 solved ...

--- Day00/srcs/exercises/ex00/ft_hello_garden.js
+++ Day00/srcs/exercises/ex00/ft_hello_garden.js
@@ -1,5 +1,20 @@
+/* Function declaration */
 function ft_hello_garden() {
   console.log("Hello , Garden Community !");
 }
 
 module.exports = { ft_hello_garden };
+
+/* Function expression */
+// const func = function (name) {
+//   console.log(`hello ${name}`);
+// };
+
+// func("karim");
+/*  Arrow function */
+// const say_hello = (name) => {
+//   console.log(`hello ${name}`);
+// };
+
+// say_hello("ahmed");
+
