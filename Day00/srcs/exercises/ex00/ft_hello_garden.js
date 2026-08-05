6a5ba3181f4a4486e6cdd891eea499e6506985b9
Ayoub Adouay <aadouay>
2026-08-05 11:44:21 +0100

    ex00 updated ..

--- Day00/srcs/exercises/ex00/ft_hello_garden.js
+++ Day00/srcs/exercises/ex00/ft_hello_garden.js
@@ -18,3 +18,20 @@ module.exports = { ft_hello_garden };
 
 // say_hello("ahmed");
 
+/* example void func vs return func ! */
+
+// const add_numbers = (a, b) => {
+//     console.log(a + b);
+// };
+
+// const return_value_of_sum = (a, b) => {
+//   return a + b;
+// };
+
+// console.log("test the void function !");
+// add_numbers(14, 10);
+
+// console.log("test the function that has a parameters !");
+
+// let res = return_value_of_sum(20, 10);
+// console.log(res);
