b8d6903565658397d3a286a31ad2e325423b47bd
Ayoub Adouay <aadouay>
2026-08-07 09:41:18 +0100

    new concepts type of arraw func

--- Day01/srcs/exercises/ex00/ft_slice_split.js
+++ Day01/srcs/exercises/ex00/ft_slice_split.js
@@ -7,10 +7,14 @@ function ft_slice_split(sentence) {
     const words = sentence.split(" ")
     const first_word = words.slice(0, 1)
     console.log(`First word: ${first_word}`)
-    const motsAvecQuotes = words.map(s => `'${s}'`)
+    const motsAvecQuotes = words.map((s) => `'${s}'`)
+    // in the arraw function we don't need to write the { return }
+    // we have to types Implicit Return and Explicit Return (required with {})
+    // check reserces of day00 for good understanding !
+    // const motsAvecQuotes = words.map((s) => {return `'${s}'`})
     console.log(`Words: [ ${motsAvecQuotes.join(", ")} ]`)
 }
 
 // ft_slice_split("Node handles many requests");
 
-module.exports = { ft_slice_split };
+module.exports = { ft_slice_split }
